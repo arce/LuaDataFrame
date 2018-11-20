8317ab862608d1f1bb7b007e909c7dfdfd759e05
Armando Arce <armando.arce@gmail.com>
2018-11-19 21:16:39 -0600

    CSV reader added

--- dataFrame.lua
+++ dataFrame.lua
@@ -1,4 +1,6 @@
+local write = io.write
 require "dataVector"
+require "CSV"
 
 function dataFrame(args)
   local table = args or {}
@@ -9,7 +11,7 @@ function dataFrame(args)
       _table[k] = v
     end,
     __index = function(table,k)
-      return _table[k]
+     return _table[k]
     end
   }
   table.size = function()
@@ -24,4 +26,40 @@ function dataFrame(args)
   end
   setmetatable(table,metatable)
   return table
+end
+
+function printDataFrame(dfrm)
+  local names = dfrm.names()
+  for i=1,names.size() do
+      write(names[i].." ")
+  end
+  write("\n")
+  for i=1,dfrm[names[1]].size() do
+    for j=1,names.size() do
+      write(tostring(dfrm[names[j]][i]).." ")
+    end
+    write("\n")
+  end
+end
+
+function readCSV(file, header, sep)
+  local dfrm = dataFrame()
+  local fd = io.open(file)
+  local line = fd:read()
+  local headers = fromCSV(line)
+  for i=1,#headers do
+    dfrm[headers[i]] = vector()
+  end
+  local register = nil
+  local count = 1
+  local line = fd:read()
+  while line ~= nil do
+    register = fromCSV(line)
+    for i=1,#register do
+      dfrm[headers[i]][count] = register[i]
+    end
+    line = fd:read()
+    count = count + 1
+  end
+  return dfrm
 end
--- test.lua
+++ test.lua
@@ -3,11 +3,13 @@ require "dataFrame"
 n = c{2, 3, 5}
 s = c{"aa", "bb", "cc"}
 b = c{true, false, true}
-df = dataFrame{primero=n,segundo=s,tercero=b} -- df is a data frame
- 
-for i = 1,df.size() do
-  printVector(df[i])
-end
 
-print(df[1][2])
-printVector(df.names())
+dfrm1 = dataFrame{NUMBER=n,STRING=s,BOOLEAN=b}
+ 
+printDataFrame(dfrm1)
+
+print()
+
+dfrm2 = readCSV("data.csv", true, ",")
+
+printDataFrame(dfrm2)
