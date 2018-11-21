b73546d236f7b134d2c46d53f44a89a8e1438b77
Armando Arce <armando.arce@gmail.com>
2018-11-20 21:13:09 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,23 +1,26 @@
 require "dataFrame"
 
-n = c{2, 3, 5}
-s = c{"aa", "bb", "cc"}
-b = c{true, false, true}
-
-dfrm1 = dataFrame{numbers=n,strings=s,booleans=b}
+local dfrm1 = dataFrame("Test1")
+dfrm1.addRow{name="juan",age=23,weight=176}
+dfrm1.addRow{name="jose",age=32,weight=250}
+dfrm1.addRow{name="pedro",age=29,weight=210}
 
 printDataFrame(dfrm1)
-
 print()
 
-dfrm2 = dataFrame("Test2")
-dfrm2['numbers'] = n
-dfrm2['strings'] = s
-dfrm2['booleans'] = b
- 
+local dfrm2 = dataFrame("Test2")
+dfrm2.addColumns{"name","age","weight"}
+dfrm2['name'][1] = "juan"
+dfrm2['age'][1] = 23
+dfrm2['weight'][1] = 176
+
+dfrm2['name'][2] = "jose"
+dfrm2['age'][2] = 32
+dfrm2['weight'][2] = 250
+
 printDataFrame(dfrm2)
-
 print()
+
 
 dfrm3 = readCSV("data.csv", true, ",")
 
